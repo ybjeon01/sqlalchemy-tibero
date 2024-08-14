9207f5499d03d890e4b6e9157fc75ca5c6a88287
Yeongbae_jeon <ybjeon01@naver.com>
2024-08-14 18:12:48 +0900

    테스트 runner 사용 추가 및 tbodbc 로깅 관련 환경 변수 추

--- pytest_runner.py
+++ pytest_runner.py
@@ -3,10 +3,15 @@ import pytest
 import os
 
 os.environ["TB_HOME"] = "/media/tibero/data/tibero/tibero7/repos/develop"
-# os.environ["TBCLI_LOG_LVL"] = "TRACE"
-# os.environ["TBCLI_LOG_DIR"] = "/tmp"
+os.environ["TBCLI_LOG_LVL"] = "TRACE"
+os.environ["TBCLI_LOG_DIR"] = "/tmp"
 
 def main():
+    # result = pytest.main(["--db", "tibero", "test/test_suite.py::UnicodeVarcharTest"])
+    # result = pytest.main(["--db", "tibero", "test/test_suite.py::UnicodeVarcharTest::test_empty_strings_varchar"])
+    # result = pytest.main(
+    #    ["--db", "tibero", "test/test_suite.py::ComponentReflectionTest::test_comments_unicode_full"])
+    # result = pytest.main(["--db", "tibero", "test/test_suite.py::ComponentReflectionTest::test_get_multi_table_comment"])
     result = pytest.main(["--db", "tibero"])
 
     if result == 0:

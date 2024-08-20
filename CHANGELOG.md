63e5782166cd10d0e43d4008fe04e00c3cbe122c
Yeongbae_jeon <ybjeon01@naver.com>
2024-08-20 15:01:19 +0900

    dialect 미구현 메서드 구현 및 버그 수정
    - get_isolation_level() 메서드 구현
    - set_isolation_level() 메서드에서 AUTOCOMMIT 인자를 사용할 때
    발생하는 버그 수정

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+
+# Change Log
+All notable changes to this project will be documented in this file.
+ 
+The format is based on [Keep a Changelog](http://keepachangelog.com/)
+and this project adheres to [Semantic Versioning](http://semver.org/).
+ 
+## [2.0.0a2] - 2024-08-20
+ 
+Here we write upgrading notes for brands. It's a team effort to make them as
+straightforward as possible.
+ 
+### Added
+ 
+### Changed
+- get_isolation_level() 메서드 구현 완료
+- AUTHORS에 작성자 이름 변경
+
+### Fixed
+- set_isolation_level() 메서드 인자가 AUTOCOMMIT일 때 예외가 발생하던 문제 해결 완료
--- sqlalchemy_tibero/pyodbc.py
+++ sqlalchemy_tibero/pyodbc.py
@@ -21,6 +21,7 @@ from typing import cast
 import pyodbc
 
 from sqlalchemy.engine import interfaces
+from sqlalchemy import exc
 from sqlalchemy.engine.interfaces import DBAPIConnection, IsolationLevel
 from .base import TiberoExecutionContext, TiberoDialect
 from sqlalchemy.connectors.pyodbc import PyODBCConnector
@@ -92,13 +93,62 @@ class TiberoDialect_pyodbc(PyODBCConnector, TiberoDialect):
     def get_isolation_level(
         self, dbapi_connection: DBAPIConnection
     ) -> IsolationLevel:
-        # TODO: 네트워크통해서 현재 isolation level을 가져오기
-        #       티베로 jdbc 코드에서는 set_isolation_level() 메서드를 사용함으로서
-        #       get_isolation_level() 메서드를 구현하는데 내가 (전영배)가 보기에는 올바른
-        #       방법이 아닙니다. 사용자가 get_isolation_level()를 통해 현재
-        #       isolation level을 볼려고 하는데 set_isolation_level을 통해 상태가 변경되면
-        #       예상치 못한 결과가 발생할 수 있습니다.
-        return "READ COMMITTED"
+        # general idea of transaction id, have to start one, etc.
+        # https://stackoverflow.com/questions/10711204/how-to-check-isoloation-level
+
+        # how to decode xid cols from v$transaction to match
+        # https://asktom.oracle.com/pls/apex/f?p=100:11:0::::P11_QUESTION_ID:9532779900346079444
+
+        # Tibero v$transaction document
+        # https://technet.tmax.co.kr/upload/download/online/tibero/pver-20220224-000002/index.html
+
+        # this is the only way to ensure a transaction is started without
+        # actually running DML. There's no way to see the configured
+        # isolation level without getting it from v$transaction which
+        # means transaction has to be started.
+        cursor = dbapi_connection.cursor()
+        try:
+            # 임시 함수 get_trans_id_34218484 생성 / 34218484는 임의의 숫자입니다.
+            # oracle은 out parameter를 지원하지만 pyodbc에서는 불가능한 것 같습니다.
+            # 이로 인해 편법으로 임시 함수를 만들고 select function() from dual 형식으로
+            # 사용 했습니다.
+
+            # 티베로에는 local_transaction_id 함수가 존재하나 문서가 없습니다. 언제든 스펙이
+            # 바뀔 수 있다는 문제가 있습니다.
+            cursor.execute("""
+                CREATE FUNCTION get_trans_id_34218484 RETURN VARCHAR IS
+                    trans_id VARCHAR(100);
+                BEGIN
+                    trans_id := dbms_transaction.local_transaction_id(TRUE);
+                    RETURN trans_id;
+                END;
+            """)
+
+            cursor.execute("SELECT get_trans_id_34218484 FROM dual")
+            trans_id = cursor.fetchone()[0]
+            xidusn, xidslot, xidsqn = trans_id.split(".", 2)
+            # 티베로의 여러 view를 보면 (xidusn, xidslot, xidsqn) 또는 (usn, slot, wrap) 칼럼명을 씁니다.
+            # 이를 보아 이름의 통일성이 없는 문제가 있습니다.
+            cursor.execute(
+                """
+                SELECT CASE flag
+                WHEN 0 THEN 'SERIALIZABLE'
+                ELSE 'READ COMMITTED' END AS isolation_level
+                FROM v$transaction WHERE
+                usn = ? AND slot = ? AND wrap = ?
+                """, (xidusn, xidslot, xidsqn)
+            )
+            row = cursor.fetchone()
+            if row is None:
+                raise exc.InvalidRequestError(
+                    "could not retrieve isolation level"
+                )
+            result = row[0]
+        finally:
+            cursor.execute("DROP FUNCTION GET_TRANS_ID_34218484")
+            cursor.close()
+
+        return result
 
     def set_isolation_level(
         self,
@@ -107,11 +157,11 @@ class TiberoDialect_pyodbc(PyODBCConnector, TiberoDialect):
     ) -> None:
         if level == "AUTOCOMMIT":
             dbapi_connection.autocommit = True
+        else:
+            supported_levels = self.get_isolation_level_values(dbapi_connection)
+            assert level in supported_levels, f"{level} is an unsupported isolation level"
 
-        supported_levels = self.get_isolation_level_values(dbapi_connection)
-        assert level in supported_levels, f"{level} is an unsupported isolation level"
-
-        dbapi_connection.autocommit = False
-        cursor = dbapi_connection.cursor()
-        cursor.execute(f"ALTER SESSION SET ISOLATION_LEVEL={level}")
-        cursor.commit()
+            dbapi_connection.autocommit = False
+            cursor = dbapi_connection.cursor()
+            cursor.execute(f"ALTER SESSION SET ISOLATION_LEVEL={level}")
+            cursor.commit()
